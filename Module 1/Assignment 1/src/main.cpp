a0b860102e029bf7e77ece15f14d03d4a8c44cbf
Connor Andrew Taylor <cat@Connors-MacBook-Pro.local>
2025-01-14 17:26:57 -0500

    Prompt user for cube dimensions and validate

--- Module 1/Assignment 1/src/main.cpp
+++ Module 1/Assignment 1/src/main.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
+#include <cstring>
 
-int main() 
+#include "Cube.h"
+
+using std::cout, std::cin;
+
+bool validateCubeDimension(int dimension, char *message)
+{
+    if (cin.fail())
+    {
+        std::cout << message;
+        return -false;
+    }
+
+    return dimension > 0;
+}
+
+int main()
 {
 
-    std::cout << "All setup!\n";
+    int length, width, height;
+
+    cout << "Enter cube length: ";
+    cin >> length;
+
+    if (!validateCubeDimension(length, "Make sure the length is a whole integer > 0!"))
+    {
+        return -1;
+    }
+
+    cout << "Enter cube width: ";
+    cin >> width;
+
+    if (!validateCubeDimension(width, "Make sure the width is a whole integer > 0!"))
+    {
+        return -1;
+    }
+
+    cout << "Enter cube height: ";
+    cin >> height;
+
+    if (!validateCubeDimension(height, "Make sure the height is a whole integer > 0!"))
+    {
+        return -1;
+    }
+
+    char colorBuffer[1024];
+
+    cout << "Enter cube color: ";
+    cin >> colorBuffer;
+
+    Cube cube(length, width, height, colorBuffer);
 
 }
